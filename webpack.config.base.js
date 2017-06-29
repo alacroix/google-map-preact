a1bb0131f50a07023f5b3b0e1616ab3cdb527f0b
Adrien Lacroix <adrienla@theodo.fr>
2017-06-29 13:53:44 +0200

    switch to preact

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,28 +4,34 @@ var reactExternal = {
   root: 'React',
   commonjs2: 'react',
   commonjs: 'react',
-  amd: 'react'
+  amd: 'react',
 };
 
 var reactDomExternal = {
   root: 'ReactDOM',
   commonjs2: 'react-dom',
   commonjs: 'react-dom',
-  amd: 'react-dom'
+  amd: 'react-dom',
 };
 
 module.exports = {
   output: {
     library: 'GoogleMapReact',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   externals: {
-    'react': reactExternal,
+    react: reactExternal,
     'react-dom': reactDomExternal,
   },
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
-    ]
+    loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel' }],
+  },
+  resolve: {
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+      // Not necessary unless you consume a module using `createClass`
+      'create-react-class': 'preact-compat/lib/create-react-class',
+    },
   },
 };
